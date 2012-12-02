788d94d3877bcece7f616f07ac6d99ab41198190
Victor Schroder <schrodervictor@gmail.com>
2012-12-02 19:48:05 -0200

    Redacao inicial da classa SimpleChatClient

--- src/SimpleChatClient.java
+++ src/SimpleChatClient.java
@@ -1,12 +1,63 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Victor Schroder
  */
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+
 public class SimpleChatClient {
+    JTextField outgoing;
+    PrintWriter writer;
+    Socket socket;
+    
+    public void go() {
+        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainPanel = new JPanel();
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        setUpNetworking();
+        frame.setSize(400,500);
+        frame.setVisible(true);
+        
+        
+    }
+
+    private void setUpNetworking() {
+        try {
+            socket = new Socket("127.0.0.1", 5000);
+            writer = new PrintWriter(socket.getOutputStream());
+            System.out.println("Networking established");    
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new SimpleChatClient().go();
+    }
+    
+    class SendButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                writer.println(outgoing.getText());
+                writer.flush();
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+        
+    }
     
 }
